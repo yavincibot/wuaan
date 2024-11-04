5ef82d50dcd392ca821738d365ed231f9971e4b1
anmol <aashishanmol.14.com>
2024-11-04 10:25:16 +0530

    first commit

--- dist/scenes/addAIO/aIODocument.js
+++ dist/scenes/addAIO/aIODocument.js
@@ -34,38 +34,29 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-import database from "../../services/database.js";
+import getRandomId from "../../extra/getRandomId.js";
 export default function getDramadata(dramaDetails, messageIds) {
     return __awaiter(this, void 0, void 0, function () {
-        var sortData, shareId, sortLink, error_1;
+        var shareId;
         return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    _a.trys.push([0, 2, , 3]);
-                    return [4 /*yield*/, database.removeFirstItem()];
-                case 1:
-                    sortData = _a.sent();
-                    if (!sortData) {
-                        throw new Error("Failed to retrieve sort data");
-                    }
-                    shareId = sortData.shareId;
-                    sortLink = sortData.aioShortUrl;
-                    return [2 /*return*/, {
-                            shareId: shareId,
-                            messageIds: messageIds || [],
-                            aIOTitle: dramaDetails.aIOTitle || "",
-                            aIOPosterID: dramaDetails.aIOPosterID || "0",
-                            aioShortUrl: sortLink || "null",
-                            episodes: [
-                                { episodeNumber: 1, shortUrl: "DummyLink1", teleUrl: "DummyURL1" },
-                                { episodeNumber: 2, shortUrl: "DummyLink2", teleUrl: "DummyURL2" },
-                            ],
-                        }];
-                case 2:
-                    error_1 = _a.sent();
-                    return [2 /*return*/, null];
-                case 3: return [2 /*return*/];
+            try {
+                shareId = parseInt(getRandomId().toString().slice(3));
+                return [2 /*return*/, {
+                        shareId: shareId,
+                        messageIds: messageIds || [],
+                        aIOTitle: dramaDetails.aIOTitle || "",
+                        aIOPosterID: dramaDetails.aIOPosterID || "0",
+                        aioShortUrl: "null",
+                        episodes: [
+                            { episodeNumber: 1, shortUrl: "DummyLink1", teleUrl: "DummyURL1" },
+                            { episodeNumber: 2, shortUrl: "DummyLink2", teleUrl: "DummyURL2" },
+                        ],
+                    }];
             }
+            catch (error) {
+                return [2 /*return*/, null];
+            }
+            return [2 /*return*/];
         });
     });
 }
